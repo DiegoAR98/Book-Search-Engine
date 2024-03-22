2495b036ab9e3948ecf0f631a80267c31a3e8b06
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-03-22 17:38:09 -0400

    Wrap the application with ApolloProvider to connect with Apollo Server.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
-import './App.css';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import Navbar from './components/Navbar';
+import './App.css';
+
+// Initialize ApolloClient
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    // Use ApolloProvider to make the Apollo Client available to all child components
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
 }
 
