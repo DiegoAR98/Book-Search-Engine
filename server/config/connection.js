4ff405c90c33e74592e0855c1c8746b114dde3d4
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-03-23 12:31:23 -0400

    change string on env file

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Load environment variables from .env file
 require('dotenv').config();
 
-// Remove the fallback for testing purposes
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
