6fdb45e50009b68de1a258bacacf7d21482771f1
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-03-24 15:59:41 -0400

    change server js to server static

--- server/server.js
+++ server/server.js
@@ -26,15 +26,15 @@ async function startApolloServer(typeDefs, resolvers) {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
-  // Serve up static assets
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-  }
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+}
 
-  // Serve the Apollo Server on the same path as the Express server
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
+// Serve the Apollo Server on the same path as the Express server
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
   db.once('open', () => {
     app.listen(PORT, () => {
