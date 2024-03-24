5dcc2ba0a1b6a081e907d01332f25574e44e87f5
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-03-24 15:52:58 -0400

    small change to redeloy on render again

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false 
+  });
+  
 
 module.exports = mongoose.connection;
