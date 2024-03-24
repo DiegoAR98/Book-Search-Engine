c97687939990e80b1e273cdec78ff494a73f1500
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-03-24 16:32:08 -0400

    add env to server js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
@@ -18,11 +19,11 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
