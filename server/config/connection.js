d5493fd8454dfaf5413a3eaf17a6fb9f3cd609aa
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-03-24 16:47:26 -0400

    move env to the root

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 // Load environment variables from .env file
 require('dotenv').config();
 
+
 mongoose.connect(process.env.MONGODB_URI);
+
 
 module.exports = mongoose.connection;
