098c95343fdd09de96f2b781bae10a44a884802f
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-03-24 17:19:51 -0400

    fixed path again

--- server/server.js
+++ server/server.js
@@ -18,13 +18,13 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// if we're in production, serve client/dist as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, './client/dist')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/dist/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
